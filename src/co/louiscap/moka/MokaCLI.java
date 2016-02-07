717ebacdf593a17b1b0d5c696c732469b98f1e47
Louis Capitanchik <ljcapitanio@gmail.com>
2016-02-07 02:19:59 +0000

    Added CLI args libs

--- src/co/louiscap/moka/MokaCLI.java
+++ src/co/louiscap/moka/MokaCLI.java
@@ -1,47 +1,86 @@
 /*
-Copyright (c) 2015, Louis Capitanchik
-All rights reserved.
+ Copyright (c) 2015, Louis Capitanchik
+ All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
 
-* Neither the name of Affogato nor the names of its associated properties or
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
+ * Neither the name of Affogato nor the names of its associated properties or
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package co.louiscap.moka;
 
+import co.louiscap.moka.util.io.MultiplexedStringPrinter;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
- * The main application entry point when invoked from the command line.
- * Handles parsing of command line arguments and delegation to other Moka
- * components
+ * Main entry point for Moka compiler. Parses options and invokes modules based
+ * upon inputs.
  * @author Louis Capitanchik
  */
 public class MokaCLI {
 
+    public static final MultiplexedStringPrinter PRINTER = new MultiplexedStringPrinter();
+    public static CommandLine PROGOPTS;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        PRINTER.addChannel("out", System.out);
+        PRINTER.addChannel("err", System.err);
+        
+        CommandLineParser cliparse = new DefaultParser();
+        Options opts = setupCommandLine();
+        try {
+            PROGOPTS = cliparse.parse(opts, args);
+        } catch (ParseException ex) {
+            PRINTER.println(ex.getMessage(), "err");
+        }
+        
+        if(PROGOPTS.hasOption("h")) {
+            HelpFormatter hf = new HelpFormatter();
+            hf.printHelp("Moka", opts, true);
+            System.exit(0);
+        }
+        
+        if(PROGOPTS.hasOption("v")) {
+            PRINTER.addChannel("debug", System.out);
+        }
+        
+        System.exit(312);
+    }
+    
+    private static Options setupCommandLine() {
+        Options options = new Options();
+        options.addOption("v", false, "Verbose; Print debug info to stdout");
+        options.addOption("o", "out", true, "Outfile; the compilation target");
+        options.addOption("e", true, "File encoding. Defaults to UTF-8");
+        options.addOption("h", "help", false, "Print out this help text");
+        return options;
     }
     
 }
---
+++ src/co/louiscap/moka/util/io/MultiplexedStringPrinter.java
@@ -0,0 +1,191 @@
+/*
+ Copyright (c) 2015, Louis Capitanchik
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ * Neither the name of Affogato nor the names of its associated properties or
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package co.louiscap.moka.util.io;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * A class for printing strings to multiple different streams
+ * @author Louis Capitanchik &lt;contact@louiscap.co&gt;
+ */
+public class MultiplexedStringPrinter {
+    private final HashMap<String, PrintStream> channels;
+    
+    /**
+     * Creates a new empty MultiplexedStringPrinter, useful for printing one string to multiple output
+     * sources
+     */
+    public MultiplexedStringPrinter() {
+        this.channels = new HashMap<>();        
+    }
+    
+    /**
+     * Creates a new MultiplexedStringPrinter based on an existing HashMap, useful for printing one 
+     * string to multiple output sources
+     * @param channels A HashMap containing tagged print streams to form the basis of this printer
+     */
+    public MultiplexedStringPrinter(HashMap<String, PrintStream> channels){
+        this.channels = channels;
+    }
+    
+    /**
+     * Add a new output that can be written to.
+     * @param tag A unique name for this print stream, so that it can be issued individual commands
+     * @param channel The print stream to add to this String Printer
+     */
+    public void addChannel (String tag, PrintStream channel) {
+        channels.put(tag, channel);
+    }
+    
+    /**
+     * Remove one or more print stream(s) from this String Printer if it is no longer needed
+     * @param tags The names of the channels to remove
+     */
+    public void removeChannels (String... tags){
+        Arrays.stream(tags).forEach((tag) -> channels.remove(tag));
+    }
+    
+    /**
+     * Print a string and then a newline to every print stream in this MultiplexedStringPrinter
+     * @param s The string to print out
+     * @see java.io.PrintStream#println(java.lang.String)
+     */
+    public void println(String s) {
+        channels.entrySet().forEach(e -> e.getValue().println(s));
+    }
+    
+    /** 
+     * Print a string and then a newline to the specified channels in this MultiplexedStringPrinter
+     * @param s The string to print out
+     * @param tags The names of the channels to print to. Null channels will be ignored
+     * @see java.io.PrintStream#println(java.lang.String)
+     */
+    public void println(String s, String... tags) {
+        Arrays.stream(tags).forEach((tag) -> {
+            PrintStream p = channels.get(tag);
+            if (p != null) {
+                p.println(s);
+            }
+        });
+    }
+    
+    /**
+     * Print a string to every print stream in this MultiplexedStringPrinter
+     * @param s The string to print out
+     * @see java.io.PrintStream#print(java.lang.String)
+     */
+    public void print(String s) {
+        channels.entrySet().forEach(e -> e.getValue().print(s));
+    }
+    
+    /**
+     * Print a string to the specified channels in this MultiplexedStringPrinter
+     * @param s The string to print out
+     * @param tags The names of the channels to print to
+     * @see java.io.PrintStream#print(java.lang.String)
+     */
+    public void print(String s, String... tags) {
+        Arrays.stream(tags).forEach((tag) -> {
+            PrintStream p = channels.get(tag);
+            if (p != null) {
+                p.print(s);
+            }
+        });
+    }
+
+    /**
+     * Close all the print streams in this MultiplexedStringPrinter and flush their contents
+     * @see java.io.PrintStream#close() 
+     */
+    public void close() {
+        channels.entrySet().forEach(e -> e.getValue().close());
+    }
+    
+    /**
+     * Close the specified print streams in this MultiplexedStringPrinter and flush their contents
+     * @param tags The names of the print streams to close
+     * @see java.io.PrintStream#close()
+     */
+    public void close(String... tags) {
+        Arrays.stream(tags).forEach((tag) -> {
+            PrintStream p = channels.get(tag);
+            if (p != null) {
+                p.close();
+            }
+        });
+    }
+
+    /**
+     * Flush the contents of all print streams in this MultiplexedStringPrinter
+     * @see java.io.PrintStream#flush() 
+     */
+    public void flush() {
+        channels.entrySet().forEach(e -> e.getValue().flush());
+    }
+    
+    /**
+     * Flush the contents of the specified print streams in this MultiplexedStringPrinter
+     * @param tags The names of the print streams to flush
+     * @see java.io.PrintStream#flush() 
+     */
+    public void flush(String... tags) {
+        Arrays.stream(tags).forEach((tag) -> {
+            PrintStream p = channels.get(tag);
+            if (p != null) {
+                p.flush();
+            }
+        });
+    }
+    
+    /**
+     * Gets a subset of the current print streams in this MultiplexedStringPrinter and returns them
+     * wrapped in a new MultiplexedStringPrinter. <br>
+     * For example, if you had a MultiplexedStringPrinter with 15 channels, but only wanted to print 
+     * to three labelled "out", "debug" and "file" repeatedly without having to provide the tags every
+     * time, you could batch them into their own MultiplexedStringPrinter and call the bare methods
+     * such as {@link MultiplexedStringPrinter#print(String)} and 
+     * {@link MultiplexedStringPrinter#flush()} by  calling 
+     * {@code example.getBatch("out", "debug", "file")}
+     * @param tags The names of the channels to batch
+     * @return A new MultiplexedStringPrinter containing only the specified print streams
+     */
+    public MultiplexedStringPrinter getBatch(String... tags) {
+        HashMap<String, PrintStream> batch = new HashMap<>();
+        Arrays.stream(tags).forEach((tag) -> {
+            PrintStream cur = channels.get(tag);
+            if(cur != null) {
+                batch.put(tag, cur);
+            }
+        });
+        return new MultiplexedStringPrinter(batch);
+    }
+}
